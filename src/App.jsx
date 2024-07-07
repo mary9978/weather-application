b28f9951c1b9bdecfbd36680a8419ea5b5ea8d9b
ftmhabt <ftmh.a78@gmail.com>
2024-07-07 17:47:45 +0330

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import DailyTips from "./components/daily-tips/daily-tips";
+import WeatherDisplay from "./components/weather-display/weather-display";
 import WeatherForecast from "./components/WeatherForecast";
 import DarkModeProvider from "./context/DarkModeContext";
 import AppLayout from "./pages/AppLayout";
@@ -7,8 +9,9 @@ function App() {
    <DarkModeProvider>
       <AppLayout>
         <div className="container xl:max-w-screen-xl mx-auto">
+          <WeatherDisplay/>
           <WeatherForecast />
-          
+          <DailyTips/>
         </div>
       </AppLayout>
    </DarkModeProvider>
