8f7146ee30173e40541f45e479e8bd9a1a2c8c64
ftmhabt <ftmh.a78@gmail.com>
2024-07-10 21:40:25 +0330

    fix image ratio

--- src/components/weather-display/weather-display.jsx
+++ src/components/weather-display/weather-display.jsx
@@ -4,7 +4,7 @@ import cloudy from "../../assets/cloudy.png";
 export default function WeatherDisplay() {
   return (
     <div className="flex flex-col lg:flex-row-reverse justify-evenly lg:items-center text-[#fff] bg-[color:var(--color-primary-1)] p-10 md:p-32 lg:py-10 lg:px-0 mx-auto max-w-screen lg:bg-hero-bg lg:bg-cover">
-      <img src={cloudy} alt="cloudy" className="max-h-[360px] lg:invisible"/>
+      <img src={cloudy} alt="cloudy" className="max-h-[360px] lg:invisible mx-auto"/>
       <WeatherData />
     </div>
   );
